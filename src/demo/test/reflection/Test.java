ce0391bd125ec9cf7a9ea7e7263061887c6403f0
pintugorai <pintubrsnc@gmail.com>
2019-02-14 18:59:24 +0530

    Changes

--- src/demo/test/reflection/Test.java
+++ src/demo/test/reflection/Test.java
@@ -2,27 +2,29 @@ package demo.test.reflection;
 
 public class Test {
 	// creating a private field
-    private String s;
- 
-    // creating a public constructor
-    public Test()  {  s = "Pintu"; }
- 
-    // Creating a public method with no arguments
-    public void method1()  {
-        System.out.println("Method1: The string is " + s);
-    }
-    
-    public void method11() {
-        System.out.println("method11");
-    }
- 
-    // Creating a public method with int as argument
-    public void method2(int n)  {
-        System.out.println("The number is = " + n);
-    }
- 
-    // creating a private method
-    private void method3() {
-        System.out.println("Private method invoked");
-    }
+	private String s;
+
+	// creating a public constructor
+	public Test() {
+		s = "Pintu";
+	}
+
+	// Creating a public method with no arguments
+	public void method1() {
+		System.out.println("Method1: The string is " + s);
+	}
+
+	public void method11() {
+		System.out.println("method11");
+	}
+
+	// Creating a public method with int as argument
+	public void method2(int n) {
+		System.out.println("The number is = " + n);
+	}
+
+	// creating a private method
+	private void method3() {
+		System.out.println("Private method invoked");
+	}
 }
